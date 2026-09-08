bbbe1c6c4f182b09bcf91ed186cc4542da679aa5
abdulrehman192005@gmail.com <ABD19363@gmail.com>
2026-09-08 20:06:41 +0500

    Fix the allow_origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,11 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:3001",
-        "http://127.0.0.1:3001",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "https://inventory-invoicing-app.vercel.app"
+        "https://inventory-invoicing-app.vercel.app/"
     ],
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
